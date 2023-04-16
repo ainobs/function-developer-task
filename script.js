38826b5aa243fdf15cb39907c02f2c3c1338ef2e
Victor-Aienobe <viktornobe@gmail.com>
2023-04-16 22:19:43 +0100

    function developer task first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const carousel = document.querySelector('.carousel');
+const carouselBox = document.querySelectorAll('.carousel-box');
+const carouselPrev = document.querySelector('.carousel-prev');
+const carouselNext = document.querySelector('.carousel-next');
+
+let counter = 0;
+const size = carouselBox[0].clientWidth;
+
+carousel.style.transform = `translateX(-${size * counter}px)`;
+
+carouselNext.addEventListener('click', () => {
+  if (counter >= carouselBox.length - 1) return;
+  carousel.style.transition = "transform 0.5s ease-in-out";
+  counter++;
+  carousel.style.transform = `translateX(-${size * counter}px)`;
+});
+
+carouselPrev.addEventListener('click', () => {
+  if (counter <= 0) return;
+  carousel.style.transition = "transform 0.5s ease-in-out";
+  counter--;
+  carousel.style.transform = `translateX(-${size * counter}px)`;
+});
