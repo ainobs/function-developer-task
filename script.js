2717f435226d6bb6ec7887ffcbe5c2b74d49d8af
Victor-Aienobe <viktornobe@gmail.com>
2023-04-17 12:57:29 +0100

    final commit

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-const carousel = document.querySelector('.carousel');
-const carouselBox = document.querySelectorAll('.carousel-box');
+const carousel = document.getElementsByClassName('.carousel');
 
+document.addEventListener('DOMContentLoaded', () => {
 
-let counter = 0;
-const size = carouselBox[0].clientWidth;
-
-carousel.style.transform = `translateX(-${size * counter}px)`;
-
-// carouselNext.addEventListener('click', () => {
-//   if (counter >= carouselBox.length - 1) return;
-//   carousel.style.transition = "transform 0.5s ease-in-out";
-//   counter++;
-//   carousel.style.transform = `translateX(-${size * counter}px)`;
-// });
-
-// carouselPrev.addEventListener('click', () => {
-//   if (counter <= 0) return;
-//   carousel.style.transition = "transform 0.5s ease-in-out";
-//   counter--;
-//   carousel.style.transform = `translateX(-${size * counter}px)`;
-// });
+    const size = carousel.clientWidth;
+    let counter = 0;
+  
+    carousel.style.transform = `translateX(-${size * counter}px)`;
+  });
